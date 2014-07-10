99e74a9ed977a5bf93e321bbf45e86ef76e2fdbf
Tunji Sunmonu <tunji.sunmonu@gmail.com>
2014-07-10 17:15:39 +0300

    chnaged to the product unit test to reflect minitest dsl

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,37 +1,33 @@
 require 'test_helper'
 
-class ProductTest < ActiveSupport::TestCase
+describe Product do
 
-	def setup
-		@product = Product.new name: "Tunji", quantity: 4, price: 9.00
+	let(:product_params) { {name: "Tunji", quantity: 4, price: 9.00} }
+
+	let(:product) { Product.new product_params }
+
+	it "should not save if name not present"  do
+		product.name = nil
+		product.wont_be :valid?
 	end
 
-	test "should not save if name not present"  do
-		@product.name = nil
-		assert_not @product.save
+	it "should not save if quantity not present"  do
+		product.quantity = nil
+		product.wont_be :valid?
 	end
 
-	test "should not save if quantity not present"  do
-		@product.quantity = nil
-		assert_not @product.save
+	it "should not save if quantity is not integer"  do
+		product.quantity = 5.56
+		product.wont_be :valid?
 	end
 
-	test "should not save if quantity is not integer"  do
-		@product.quantity = 5.56
-		assert_not @product.save
+	it "should not save if price not present"  do
+		product.price = nil
+		product.must_be :invalid?
 	end
 
-	test "should not save if price not present"  do
-		@product.price = nil
-		assert_not @product.save
+	it "should save if all conditions are met"  do
+		product.must_be :valid?
 	end
 
-	test "should save if all conditions are met"  do
-		puts @product.price
-		assert @product.save
-	end
-
-	def teardown
-		@product = nil
-	end
 end
