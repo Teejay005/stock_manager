7230b911c7c487e61774c63e30de18ae1c25eefc
Tunji Sunmonu <tunji.sunmonu@gmail.com>
2014-07-10 15:13:38 +0300

    added test::unit suite for unit testing product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,5 @@
 class Product < ActiveRecord::Base
+	validates :name, presence: true
+	validates :quantity, presence: true, numericality: { only_integer: true}
+	validates :price, presence: true
 end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,37 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+	def setup
+		@product = Product.new name: "Tunji", quantity: 4, price: 9.00
+	end
+
+	test "should not save if name not present"  do
+		@product.name = nil
+		assert_not @product.save
+	end
+
+	test "should not save if quantity not present"  do
+		@product.quantity = nil
+		assert_not @product.save
+	end
+
+	test "should not save if quantity is not integer"  do
+		@product.quantity = 5.56
+		assert_not @product.save
+	end
+
+	test "should not save if price not present"  do
+		@product.price = nil
+		assert_not @product.save
+	end
+
+	test "should save if all conditions are met"  do
+		puts @product.price
+		assert @product.save
+	end
+
+	def teardown
+		@product = nil
+	end
 end
